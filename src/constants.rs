8c3bcd99fb8d3252802ee1e412d254bfeea16a01
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-15 23:34:30 +0700

    Create constants.rs

---
+++ src/constants.rs
@@ -0,0 +1,5 @@
+pub const MAX_SUPPLY: u64 = 9_720_000;
+pub const WERTS_PER_WGD: u64 = 100_000_000; // like satoshis
+pub const BLOCK_SIZE_MB: u64 = 8;
+pub const BLOCK_REWARD: u64 = 50;
+pub const HALVING_INTERVAL: u64 = 210_000;
