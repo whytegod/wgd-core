c3c1c5927a0f54545af22d684a87e135810e5384
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 00:20:22 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-mod block;
-mod blockchain;
-mod mining;
-
-use blockchain::Blockchain;
-use block::Block;
-
 fn main() {
-    println!("WGD Core Node Starting...");
-
-    let mut blockchain = Blockchain::new();
-
-    println!("Mining block 1...");
-    let block1 = Block::new(
-        1,
-        "First WGD block".to_string(),
-        blockchain.latest_hash(),
-    );
-
-    let mined_block = mining::mine_block(block1, 4);
-
-    blockchain.add_block(mined_block);
-
-    println!("Blockchain length: {}", blockchain.chain.len());
+    println!("WGD Core Initialized");
 }
