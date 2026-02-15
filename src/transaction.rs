5203b207b6679056542c07d87b77f24e9f2d50e0
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 04:38:21 +0700

    Delete transaction.rs

--- src/transaction.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Transaction {
-    pub sender: String,
-    pub receiver: String,
-    pub amount: u64,
-    pub timestamp: i64,
-}
