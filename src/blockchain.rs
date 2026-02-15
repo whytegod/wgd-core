319421935b91762753f61e12ea9eeba909f751dd
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-15 23:40:00 +0700

    Update blockchain.rs

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -6,13 +6,22 @@ pub struct Blockchain {
 
 impl Blockchain {
     pub fn new() -> Self {
-        let genesis_block = Block::new(0, vec![], String::from("0"));
+        let genesis = Block::new(
+            0,
+            "Genesis Block".to_string(),
+            "0".to_string(),
+        );
+
         Blockchain {
-            chain: vec![genesis_block],
+            chain: vec![genesis],
         }
     }
 
     pub fn add_block(&mut self, block: Block) {
         self.chain.push(block);
+    }
+
+    pub fn latest_hash(&self) -> String {
+        self.chain.last().unwrap().hash.clone()
     }
 }
