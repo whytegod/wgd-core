32cf13fb109f65d4b80f67628955a2176bc175e4
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 07:16:00 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,38 +2,17 @@ package main
 
 import (
 	"fmt"
-	"wgd-core/ledger"
-)
 
-const (
-	ProtocolName    = "Whytegod"
-	ProtocolVersion = "v0.3.0"
-	InitialSupply   = 9720000.0
-	BlockReward     = 10.0
+	"github.com/whytegod/wgd-core/genesis"
+	"github.com/whytegod/wgd-core/ledger"
 )
 
 func main() {
+	config := genesis.DefaultGenesis()
+	ledgerInstance := ledger.NewLedger(config.InitialSupply)
 
-	fmt.Println("====================================")
-	fmt.Println("Starting", ProtocolName, ProtocolVersion)
-	fmt.Println("====================================")
-
-	// Initialize Ledger
-	l := ledger.NewLedger(InitialSupply, BlockReward)
-
-	fmt.Println("Genesis Height:", l.Height())
-	fmt.Println("Total Supply:", l.TotalSupply())
-	fmt.Println("Treasury Balance:", l.TreasuryBalance())
-
-	// Mine Some Blocks
-	l.AddBlock("First Platinum Block")
-	l.AddBlock("Second Platinum Block")
-
-	fmt.Println("\nAfter Mining:")
-	fmt.Println("Height:", l.Height())
-	fmt.Println("Total Supply:", l.TotalSupply())
-	fmt.Println("Treasury Balance:", l.TreasuryBalance())
-
-	fmt.Println("\nFull Chain:")
-	l.PrintChain()
+	fmt.Println("Protocol:", config.ProtocolName)
+	fmt.Println("Version:", config.ProtocolVersion)
+	fmt.Println("Total Supply:", ledgerInstance.TotalSupply())
+	fmt.Println("Treasury Balance:", ledgerInstance.TreasuryBalance())
 }
