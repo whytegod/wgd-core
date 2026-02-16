95f395b205444b97176fb015dcd48a2fe1ee4914
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 08:40:08 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,15 +8,29 @@ import (
 func main() {
 	bc := core.NewBlockchain()
 
-	bc.AddBlock("First Block After Genesis")
-	bc.AddBlock("Second Block After Genesis")
+	priv1, addr1 := core.GenerateKeyPair()
+	_, addr2 := core.GenerateKeyPair()
+
+	tx := core.Transaction{
+		From:   addr1,
+		To:     addr2,
+		Amount: 10,
+	}
+
+	tx.Hash = tx.CalculateHash()
+	tx.SignTransaction(priv1)
+
+	bc.AddTransaction(tx)
+
+	fmt.Println("⛏ Mining block...")
+	bc.MinePendingTransactions()
 
 	for _, block := range bc.Blocks {
 		fmt.Println("Index:", block.Index)
-		fmt.Println("Timestamp:", block.Timestamp)
-		fmt.Println("Data:", block.Data)
-		fmt.Println("PrevHash:", block.PrevHash)
 		fmt.Println("Hash:", block.Hash)
-		fmt.Println("----------------------------------")
+		fmt.Println("Prev:", block.PrevHash)
+		fmt.Println("Nonce:", block.Nonce)
+		fmt.Println("Tx Count:", len(block.Transactions))
+		fmt.Println("------------------------")
 	}
 }
