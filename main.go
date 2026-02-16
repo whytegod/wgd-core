124c374f454f25c00e1ee7e1483e1db89dac53e0
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 08:29:43 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	bc := core.NewBlockchain()
 
-	bc.AddBlock("First Block")
-	bc.AddBlock("Second Block")
+	bc.AddBlock("First Block After Genesis")
+	bc.AddBlock("Second Block After Genesis")
 
 	for _, block := range bc.Blocks {
 		fmt.Println("Index:", block.Index)
 		fmt.Println("Timestamp:", block.Timestamp)
 		fmt.Println("Data:", block.Data)
-		fmt.Println("Prev Hash:", block.PrevHash)
+		fmt.Println("PrevHash:", block.PrevHash)
 		fmt.Println("Hash:", block.Hash)
-		fmt.Println("----------------------------")
+		fmt.Println("----------------------------------")
 	}
 }
