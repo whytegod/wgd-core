4bcbe27857d37b03fe5d29cb7210b3077a402d70
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 08:19:43 +0700

    Update block.go

--- block/block.go
+++ block/block.go
@@ -1,9 +1,8 @@
-package block
+package core
 
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"strconv"
 	"time"
 )
 
@@ -13,24 +12,22 @@ type Block struct {
 	Data      string
 	PrevHash  string
 	Hash      string
-	Reward    float64
 }
 
-func NewBlock(index int, prevHash string, data string, reward float64) Block {
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + block.Data + block.PrevHash
+	hash := sha256.Sum256([]byte(record))
+	return hex.EncodeToString(hash[:])
+}
+
+func NewBlock(index int, data string, prevHash string) Block {
 	block := Block{
 		Index:     index,
 		Timestamp: time.Now().String(),
 		Data:      data,
 		PrevHash:  prevHash,
-		Reward:    reward,
 	}
 
 	block.Hash = calculateHash(block)
 	return block
-}
-
-func calculateHash(b Block) string {
-	record := strconv.Itoa(b.Index) + b.Timestamp + b.Data + b.PrevHash
-	hash := sha256.Sum256([]byte(record))
-	return hex.EncodeToString(hash[:])
 }
