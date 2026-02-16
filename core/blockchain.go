76f0cf0e8a6bcccf324cadd135adff7c8a0bd5d9
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-16 08:20:30 +0700

    Update blockchain.go

--- core/blockchain.go
+++ core/blockchain.go
@@ -1,37 +1,18 @@
 package core
 
-import (
-	"github.com/whytegod/wgd-core/block"
-)
-
 type Blockchain struct {
-	Blocks []block.Block
+	Blocks []Block
 }
 
-// Create a new blockchain with genesis block
 func NewBlockchain() *Blockchain {
-	genesis := block.NewBlock(
-		0,
-		"0",
-		"Genesis Block - Whytegod",
-		50.0,
-	)
-
+	genesisBlock := NewBlock(0, "Genesis Block", "")
 	return &Blockchain{
-		Blocks: []block.Block{genesis},
+		Blocks: []Block{genesisBlock},
 	}
 }
 
-// Add a new block
-func (bc *Blockchain) AddBlock(data string, reward float64) {
+func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-
-	newBlock := block.NewBlock(
-		prevBlock.Index+1,
-		prevBlock.Hash,
-		data,
-		reward,
-	)
-
+	newBlock := NewBlock(prevBlock.Index+1, data, prevBlock.Hash)
 	bc.Blocks = append(bc.Blocks, newBlock)
 }
