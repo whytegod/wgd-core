97a846cf3d0d25a1fea64b30be89e7206631cb79
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-23 06:27:56 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,31 +6,22 @@ import (
 )
 
 func main() {
+	// create blockchain
 	bc := core.NewBlockchain()
 
-	priv1, addr1 := core.GenerateKeyPair()
-	_, addr2 := core.GenerateKeyPair()
-
-	tx := core.Transaction{
-		From:   addr1,
-		To:     addr2,
-		Amount: 10,
-	}
-
-	tx.Hash = tx.CalculateHash()
-	tx.SignTransaction(priv1)
-
+	// simulate adding a normal transaction (optional)
+	tx := core.NewCoinbaseTx("alice", 10)
 	bc.AddTransaction(tx)
 
-	fmt.Println("⛏ Mining block...")
-	bc.MinePendingTransactions()
-
-	for _, block := range bc.Blocks {
-		fmt.Println("Index:", block.Index)
-		fmt.Println("Hash:", block.Hash)
-		fmt.Println("Prev:", block.PrevHash)
-		fmt.Println("Nonce:", block.Nonce)
-		fmt.Println("Tx Count:", len(block.Transactions))
-		fmt.Println("------------------------")
+	// mine block
+	block, err := bc.MineBlock("miner1")
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Println("Block mined!")
+	fmt.Printf("Index: %d\n", block.Index)
+	fmt.Printf("Hash: %x\n", block.Hash)
+	fmt.Printf("PrevHash: %x\n", block.PrevHash)
+	fmt.Printf("Transactions: %d\n", len(block.Transactions))
 }
