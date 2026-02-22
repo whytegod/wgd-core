f9abb79efcdda3ad5c4188d8bc8a205fd5b71cf1
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-23 06:45:51 +0700

    Update transaction.go

--- core/transaction.go
+++ core/transaction.go
@@ -2,57 +2,43 @@ package core
 
 import (
 	"bytes"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/sha256"
 	"encoding/gob"
-	"encoding/hex"
-	"fmt"
-	"math/big"
 )
 
-type TXInput struct {
-	TxID      []byte
-	OutIndex  int
-	Signature []byte
-	PubKey    []byte
-}
-
-type TXOutput struct {
-	Value      uint64
-	PubKeyHash []byte
-}
-
 type Transaction struct {
-	ID      []byte
-	Inputs  []TXInput
-	Outputs []TXOutput
+	From      string
+	To        string
+	Amount    int
+	PublicKey []byte
+	R         []byte
+	S         []byte
 }
 
 func (tx *Transaction) Hash() []byte {
 	var encoded bytes.Buffer
-	var hash [32]byte
-
-	enc := gob.NewEncoder(&encoded)
-	_ = enc.Encode(tx)
-
-	hash = sha256.Sum256(encoded.Bytes())
-	return hash[:]
+	gob.NewEncoder(&encoded).Encode(tx.From + tx.To)
+	return encoded.Bytes()
 }
 
-func (tx *Transaction) SetID() {
-	tx.ID = tx.Hash()
-}
-
-func NewCoinbaseTx(to string, reward uint64) *Transaction {
-	txin := TXInput{TxID: []byte{}, OutIndex: -1}
-	txout := TXOutput{Value: reward, PubKeyHash: []byte(to)}
-
-	tx := Transaction{
-		Inputs:  []TXInput{txin},
-		Outputs: []TXOutput{txout},
+func NewCoinbaseTx(to string, amount int) *Transaction {
+	return &Transaction{
+		From:   "COINBASE",
+		To:     to,
+		Amount: amount,
 	}
-	tx.SetID()
-	return &tx
+}
+
+func NewTransaction(fromWallet *Wallet, to string, amount int) *Transaction {
+	tx := &Transaction{
+		From:      fromWallet.Address,
+		To:        to,
+		Amount:    amount,
+		PublicKey: fromWallet.PublicKey,
+	}
+
+	r, s := Sign(tx.Hash(), fromWallet.PrivateKey)
+	tx.R = r
+	tx.S = s
+
+	return tx
 }
