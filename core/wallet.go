36a155c80e41841c37c89238e25eea289bcc7081
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-23 06:44:23 +0700

    Update wallet.go

--- core/wallet.go
+++ core/wallet.go
@@ -4,21 +4,56 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
+	"math/big"
 )
 
 type Wallet struct {
 	PrivateKey *ecdsa.PrivateKey
-	PublicKey  string
+	PublicKey  []byte
+	Address    string
 }
 
 func NewWallet() *Wallet {
-	priv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	private, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
 
-	pub := append(priv.PublicKey.X.Bytes(), priv.PublicKey.Y.Bytes()...)
+	hash := sha256.Sum256(pubKey)
+	address := hex.EncodeToString(hash[:])
 
 	return &Wallet{
-		PrivateKey: priv,
-		PublicKey:  hex.EncodeToString(pub),
+		PrivateKey: private,
+		PublicKey:  pubKey,
+		Address:    address,
 	}
+}
+
+func Sign(data []byte, priv *ecdsa.PrivateKey) ([]byte, []byte) {
+	hash := sha256.Sum256(data)
+	r, s, _ := ecdsa.Sign(rand.Reader, priv, hash[:])
+	return r.Bytes(), s.Bytes()
+}
+
+func Verify(data, pubKey, rBytes, sBytes []byte) bool {
+	hash := sha256.Sum256(data)
+
+	x := big.Int{}
+	y := big.Int{}
+	keyLen := len(pubKey)
+	x.SetBytes(pubKey[:keyLen/2])
+	y.SetBytes(pubKey[keyLen/2:])
+
+	rawPubKey := ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     &x,
+		Y:     &y,
+	}
+
+	r := big.Int{}
+	s := big.Int{}
+	r.SetBytes(rBytes)
+	s.SetBytes(sBytes)
+
+	return ecdsa.Verify(&rawPubKey, hash[:], &r, &s)
 }
