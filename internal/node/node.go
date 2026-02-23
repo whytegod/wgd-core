6ceb33400a0cb8927c08f1706f700cb41cf90b03
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-23 09:13:11 +0700

    Update node.go

--- internal/node/node.go
+++ internal/node/node.go
@@ -2,21 +2,29 @@ package node
 
 import (
 	"sync"
+
 	"wgd-core/internal/block"
 	"wgd-core/internal/ledger"
 )
 
 type Node struct {
-	mu       sync.RWMutex
-	Chain    []*block.BlockHeader
-	UTXO     *ledger.UTXOSet
-	Mempool  map[[32]byte]interface{}
+	mu      sync.RWMutex
+	Chain   []*block.Block
+	UTXO    *ledger.UTXOSet
+	Mempool map[[32]byte]*block.Transaction
 }
 
 func NewNode() *Node {
 	return &Node{
-		Chain:   make([]*block.BlockHeader, 0),
+		Chain:   make([]*block.Block, 0),
 		UTXO:    ledger.NewUTXOSet(),
-		Mempool: make(map[[32]byte]interface{}),
+		Mempool: make(map[[32]byte]*block.Transaction),
 	}
+}
+
+func (n *Node) AddBlock(b *block.Block) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.Chain = append(n.Chain, b)
 }
