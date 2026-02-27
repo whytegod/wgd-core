55203c58431986bf1a70f4cb2ea4b8a0ac6bbcfc
whytegod <73440597+whytegod@users.noreply.github.com>
2026-02-27 15:39:41 +0700

    Update constants.go

--- constitutional/constants.go
+++ constitutional/constants.go
@@ -1,1 +1,12 @@
-q
+package constitutional
+
+const (
+    HardCap        uint64 = 9_720_000_000_000
+    GenesisSupply  uint64 = 1_458_000_000_000
+    InitialReward  uint64 = 197_000
+    HalvingInterval uint64 = 21_024_000
+    MaxEras        uint64 = 8
+    BurnRatio      uint64 = 30
+)
+
+const ConstitutionalVersion = "1.0"
